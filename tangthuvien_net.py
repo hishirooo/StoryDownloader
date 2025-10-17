c0648f5a955f44ad93b3cebf275cce3f8ca98cb3
Cá Mụp Cắn Cáp <69391426+hishirooo@users.noreply.github.com>
2025-10-17 08:03:06 +0700

    Lấy thông tin truyện.

--- tangthuvien_net.py
+++ tangthuvien_net.py
@@ -16,11 +16,16 @@ from typing import Optional
 def getinfo(url: str) -> dict:
     """Lấy thông tin truyện và danh sách chương từ tangthuvien.vn"""
     soup = _fetch_html(url)
-    book_info = _get_book_info(soup)
+    book_info = _get_book_info(soup) # "title": title, "author": author, "genre": genre, "status": status
     chapters = _get_list_chapters(soup)
     #book_info["chapters"] = chapters
     #
-    print(f"Đã lấy {len(chapters)} chương.")
+    print("-----------------THÔNG TIN TRUYỆN:-------------------")
+    print(f"Title :  {book_info['title']}")
+    print(f"Author: {book_info['author']}")
+    print(f"Genre :  {book_info['genre']}")
+    print(f"Status: {book_info['status']}")
+    print(f"Đã lấy {len(chapters)} link chương.")
     
 def _fetch_html(url: str) -> BeautifulSoup:
     headers = {
@@ -32,16 +37,26 @@ def _fetch_html(url: str) -> BeautifulSoup:
         r.encoding = r.apparent_encoding
     return BeautifulSoup(r.text, "html.parser")
 def _get_book_info(soup: BeautifulSoup) -> dict:
-    title = soup.select_one("h1", class_= "book-info").get_text(strip=True)
-    author_tag = soup.select_one("p",class_="tag")
-    author = author_tag.find("a",class_="blue").get_text(strip=True) if author_tag else "N/A"
-    genre = author_tag.find("a", class_="red").get_text(strip=True) if author_tag else "N/A" 
+    # title = soup.select_one("h1", class_= "book-info").get_text(strip=True)
+    # author_tag = soup.select_one("p",class_="tag")
+    # author = author_tag.find("a",class_="blue").get_text(strip=True) if author_tag else "N/A"
+    # genre = author_tag.find("a", class_="red").get_text(strip=True) if author_tag else "N/A" 
+    # #status = author_tag.select_one('div.book-info p.blue span')
+    book_info = soup.find('div', class_='book-info')
+    title = book_info.find('h1').get_text(strip=True)
+    tag_section = book_info.find('p', class_='tag')
+    author = tag_section.find('a', class_='blue').get_text(strip=True)
+    status = tag_section.find('span', class_='blue').get_text(strip=True)
+    genre = tag_section.find('a', class_='red').get_text(strip=True)
+
     return {
-         "title": title,
-         "author": author,
-         "genre": genre
+        "title": title,
+        "author": author,
+        "genre": genre,
+         "status": status
     }
     
+
 def _get_list_chapters(soup: BeautifulSoup) -> list:
     chapters = []
     chapter_list_div = soup.find("div", class_="chapter-list")
